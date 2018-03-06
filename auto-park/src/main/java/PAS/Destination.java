13009712a3d61350d975ee0f28b43ba4d67817dc
Nihal <nihalh55@gmail.com>
2018-03-07 00:03:38 +0530

    Add Layout & Destination java files

---
+++ auto-park/src/main/java/PAS/Destination.java
@@ -0,0 +1,16 @@
+//Destination Class
+//This class decribes the destinations available around the parking Layout
+
+public class Destination{
+
+    //Main class begins
+    //Data memeber declaration
+
+    private int     destination_ID;                 //holds the ID number of the destination
+    private String  destination_name;               //holds the name of the destination
+    /*variable to show position in the layout*/
+
+    //Method declarations
+
+    
+}
