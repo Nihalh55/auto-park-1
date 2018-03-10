036f4d7ac547f6fe4a0bc8287bc8c944e309a7e1
Gurupungav Narayanan <gurupungavn@gmail.com>
2018-03-10 22:41:31 +0530

    Made nice android app for SOU

---
+++ sou/app/src/main/java/com/autopark/sou/MainActivity.java
@@ -0,0 +1,169 @@
+package com.autopark.sou;
+
+import android.Manifest;
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.FileProvider;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
+import com.squareup.picasso.Picasso;
+
+import org.openalpr.OpenALPR;
+import org.openalpr.model.Results;
+import org.openalpr.model.ResultsError;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_IMAGE = 100;
+    private static final int STORAGE=1;
+    private String ANDROID_DATA_DIR;
+    private static File destination;
+    private TextView resultTextView;
+    private ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ANDROID_DATA_DIR = this.getApplicationInfo().dataDir;
+
+        findViewById(R.id.takePicBtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                checkPermission();
+            }
+        });
+        resultTextView = (TextView) findViewById(R.id.textView);
+        imageView = (ImageView) findViewById(R.id.imageView);
+
+        resultTextView.setText("Press the Button above to start.");
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE && resultCode == Activity.RESULT_OK) {
+            final ProgressDialog progress = ProgressDialog.show(this, "Loading", "Parsing result...", true);
+            final String openAlprConfFile = ANDROID_DATA_DIR + File.separatorChar + "runtime_data" + File.separatorChar + "openalpr.conf";
+            BitmapFactory.Options options = new BitmapFactory.Options();
+            options.inSampleSize = 10;
+
+            // Picasso requires permission.WRITE_EXTERNAL_STORAGE
+            Picasso.with(MainActivity.this).load(destination).fit().centerCrop().into(imageView);
+            resultTextView.setText("Processing");
+
+            AsyncTask.execute(new Runnable() {
+                @Override
+                public void run() {
+                    String result = OpenALPR.Factory.create(MainActivity.this, ANDROID_DATA_DIR).recognizeWithCountryRegionNConfig("us", "", destination.getAbsolutePath(), openAlprConfFile, 10);
+
+                    Log.d("OPEN ALPR", result);
+
+                    try {
+                        final Results results = new Gson().fromJson(result, Results.class);
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                if (results == null || results.getResults() == null || results.getResults().size() == 0) {
+                                    Toast.makeText(MainActivity.this, "License Plate Detection failed", Toast.LENGTH_LONG).show();
+                                    resultTextView.setText("No License Plate Detected");
+                                } else {
+
+                                    String prettyResult = ("\nPlate: " + results.getResults().get(0).getPlate()
+                                            // Trim confidence to two decimal places
+                                            + "\nConfidence: " + String.format("%.2f", results.getResults().get(0).getConfidence()) + "%"
+                                            // Convert processing time to seconds and trim to two decimal places
+                                            + "\nProcessing time: " + String.format("%.2f", ((results.getProcessingTimeMs() / 1000.0) % 60)) + " seconds");
+                                    resultTextView.setText(prettyResult);
+                                }
+                            }
+                        });
+
+                    } catch (JsonSyntaxException exception) {
+                        final ResultsError resultsError = new Gson().fromJson(result, ResultsError.class);
+
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                resultTextView.setText(resultsError.getMsg());
+                            }
+                        });
+                    }
+
+                    progress.dismiss();
+                }
+            });
+        }
+    }
+
+    private void checkPermission() {
+        List<String> permissions = new ArrayList<>();
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            permissions.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        }
+        if (!permissions.isEmpty()) {
+            Toast.makeText(this, "Storage access needed to manage the picture.", Toast.LENGTH_LONG).show();
+            String[] params = permissions.toArray(new String[permissions.size()]);
+            ActivityCompat.requestPermissions(this, params, STORAGE);
+        } else { // We already have permissions, so handle as normal
+            takePicture();
+        }
+    }
+
+
+    public String dateToString(Date date, String format) {
+        SimpleDateFormat df = new SimpleDateFormat(format, Locale.getDefault());
+
+        return df.format(date);
+    }
+
+    public void takePicture() {
+        // Use a folder to store all results
+        File folder = new File(Environment.getExternalStorageDirectory() + "/OpenALPR/");
+        if (!folder.exists()) {
+            folder.mkdir();
+        }
+
+        // Generate the path for the next photo
+        String name = dateToString(new Date(), "yyyy-MM-dd-hh-mm-ss");
+        destination = new File(folder, name + ".jpg");
+
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, FileProvider.getUriForFile(MainActivity.this, getString(R.string.file_provider_authority),destination));
+        startActivityForResult(intent, REQUEST_IMAGE);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (destination != null) {// Picasso does not seem to have an issue with a null value, but to be safe
+            Picasso.with(MainActivity.this).load(destination).fit().centerCrop().into(imageView);
+        }
+    }
+}
