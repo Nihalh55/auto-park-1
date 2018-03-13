37c9719d8837c5d519f3c262cc5a3c17dc46e8e4
Gurupungav Narayanan <gurupungavn@gmail.com>
2018-03-13 15:35:08 +0530

    Changes

--- Server.java
+++ Server.java
@@ -1,13 +1,16 @@
+package PAS;
+
 import java.net.*;
 import java.io.*;
- 
+
 public class Server
 {
     //initialize socket and input stream
     private Socket          socket   = null;
     private ServerSocket    server   = null;
     private DataInputStream in       = null;
- 
+    private String          message  = "";
+
     // constructor with port
     public Server(int port)
     {
@@ -15,29 +18,22 @@ public class Server
         try
         {
             server = new ServerSocket(port);
-            System.out.println("Server started");
- 
-            System.out.println("Waiting for a client ...");
- 
             socket = server.accept();
-            System.out.println("Client accepted");
- 
+
             // takes input from the client socket
             in = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
- 
-            String line = "";
+
             try
             {
-                line = in.readUTF();
-                System.out.println(line);
- 
+                message = in.readUTF();
+                // System.out.println(message);
+
             }
             catch(IOException i)
             {
                 System.out.println(i);
             }
-            System.out.println("Closing connection");
- 
+
             socket.close();
             in.close();
         }
@@ -46,7 +42,12 @@ public class Server
             System.out.println(i);
         }
     }
- 
+
+    public String getMessage()
+    {
+        return message;
+    }
+
     public static void main(String args[])
     {
         Server server = new Server(5050);
